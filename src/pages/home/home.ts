1d9632738cbbc350bd992cf9fbe537cbe65bcd17
YASSINE SABRI <yassine.sabri@edu.uca.ma>
2017-04-30 15:31:09 +0100

    add refresh interval

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component ,OnInit} from '@angular/core';
+import { Component ,OnInit,OnDestroy} from '@angular/core';
 import { NavController, App} from 'ionic-angular'; //import App
 import { AuthService } from '../../providers/auth-service';
 import { PoloniexService } from '../../providers/poloniexService';
@@ -8,7 +8,7 @@ import {LoginPage} from '../login/login';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit,OnDestroy {
 
 
   username = '';
@@ -17,10 +17,7 @@ export class HomePage implements OnInit {
   currencies : Array<String>;
   ticker : any;
   balances : any;
-  showTicker :  boolean;
-  showTickerLabel : string;
-  showBalances : boolean;
-  showBalancesLabel : string;
+  market_interval_id;
   //Inject App to get the rootNav from it and not from the NavController
   constructor(private nav: NavController, private auth: AuthService,private app:App,private poloniexService : PoloniexService) {
     let info = this.auth.getUserInfo();
@@ -28,22 +25,25 @@ export class HomePage implements OnInit {
     this.email = info['email'];
     this.markets = ['BTC_BBR','BTC_BCN','BTC_BELA','BTC_BITS','BTC_BLK','BTC_BTCD','BTC_BTM','BTC_BTS','BTC_BURST','BTC_C2','BTC_CLAM','BTC_CURE','BTC_DASH'];
     this.currencies = ['1CR','ABY','BCN','BTC','FLDC','GRC','QORA','SDC','TOR','XAP','X13','XLB'];
-    this.showTicker = false;
-    this.showTickerLabel = 'Show Tickers';
-    this.showBalances = false;
-    this.showBalancesLabel = "Show Balances";
   }
   ngOnInit(): void {
       //console.log('onInit');
-      this.poloniexService.returnTicker().subscribe(data => {
+      this.LoadMarket();
+      this.market_interval_id = setInterval(() => {
+         this.LoadMarket(); 
+         }, 2000);
+  }
+  ngOnDestroy(){
+    if (this.market_interval_id) {
+    clearInterval(this.market_interval_id);
+    }
+  }
+  LoadMarket(){
+        console.log('LoadMarket called');
+       this.poloniexService.returnTicker().subscribe(data => {
         console.log(data);
         this.ticker = data;
       });
-      this.poloniexService.returnBalances().subscribe(data => {
-        console.log(data);
-        this.balances = data;
-      });
-
   }
   doRefresh(refresher) {
       this.poloniexService.returnTicker().subscribe(data => {
@@ -55,20 +55,6 @@ export class HomePage implements OnInit {
         this.balances = data;
       });
       refresher.complete();
-  }
-  hideTicker(){
-    this.showTicker = !this.showTicker;
-    if(this.showTickerLabel == 'Hide Tickers')
-      this.showTickerLabel = 'Show Tickers';
-    else
-      this.showTickerLabel = 'Hide Tickers';
-  }
-  hideBalance(){
-    this.showBalances = !this.showBalances;
-    if(this.showBalancesLabel == 'Hide Balances')
-      this.showBalancesLabel = 'Show Balances';
-    else
-      this.showBalancesLabel = 'Hide Balances';
   }
   public logout() {
     this.auth.logout().subscribe(succ => {
