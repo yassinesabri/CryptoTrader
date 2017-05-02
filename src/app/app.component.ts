88ff2aacd20964e764b71018b89ed0f8a0cf9580
YASSINE SABRI <yassine.sabri@edu.uca.ma>
2017-05-02 21:10:37 +0100

    Add table for the currencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { LoginPage } from '../pages/login/login';
+import {TabsPage} from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = LoginPage;
+  rootPage:any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,55 +10,63 @@ import {LoginPage} from '../login/login';
 })
 export class HomePage implements OnInit,OnDestroy {
 
-
-  username = '';
-  email = '';
   markets : Array<String>;
   currencies : Array<String>;
   ticker : any;
   balances : any;
   market_interval_id;
+  market_id : string;
   //Inject App to get the rootNav from it and not from the NavController
   constructor(private nav: NavController, private auth: AuthService,private app:App,private poloniexService : PoloniexService) {
-    let info = this.auth.getUserInfo();
-    this.username = info['name'];
-    this.email = info['email'];
     this.markets = ['BTC_BBR','BTC_BCN','BTC_BELA','BTC_BITS','BTC_BLK','BTC_BTCD','BTC_BTM','BTC_BTS','BTC_BURST','BTC_C2','BTC_CLAM','BTC_CURE','BTC_DASH'];
-    this.currencies = ['1CR','ABY','BCN','BTC','FLDC','GRC','QORA','SDC','TOR','XAP','X13','XLB'];
+    this.currencies = ['AMP','ARDR','BBR','BCN','BCY','BELA','BITS',
+                      'BLK','BTC','BTCD','BTM','BTS','BURST','C2','CLAM','CURE','DASH','DCR','DGB','DOGE',
+                      'DOGE','EMC2','ETC','ETH','EXP','FCT','FLDC','FLO','GAME','GNO','GNT',
+                      'GRC','HUC','HZ','IOC','LBC','LSK','LTC','MAID','MYR','NAUT','NAV','NEOS',
+                      'NMC','NOBL','NOTE','NSR','NXC','NXT','OMNI','PASC','PINK','POT','PPC','QBK',
+                      'QORA','QTL','RADS','RBY','REP','RIC','SBD','SC','SDC','SJCX','STEEM','STR','STRAT',
+                      'SYC','UNITY','VIA','VOX','VRC','VTC','XBC','XCP','XEM','XMG','XMR','XPM','XRP','XVC','ZEC'];
+    this.market_id = 'BTC';
   }
   ngOnInit(): void {
-      //console.log('onInit');
       this.LoadMarket();
-      this.market_interval_id = setInterval(() => {
+      /*this.market_interval_id = setInterval(() => {
          this.LoadMarket(); 
-         }, 2000);
+         }, 2000);*/
   }
   ngOnDestroy(){
     if (this.market_interval_id) {
-    clearInterval(this.market_interval_id);
+      clearInterval(this.market_interval_id);
     }
   }
   LoadMarket(){
-        console.log('LoadMarket called');
+       console.log('LoadMarket called');
        this.poloniexService.returnTicker().subscribe(data => {
-        console.log(data);
+        //console.log(data);
         this.ticker = data;
       });
+      if(this.ticker){
+        for(let i=0;i<this.currencies.length;i++){
+          if(this.ticker[this.market_id+'_'+this.currencies[i]]){
+              let volume = parseFloat(this.ticker[this.market_id+'_'+this.currencies[i]].baseVolume).toFixed(3);
+              let percentage = parseFloat(this.ticker[this.market_id+'_'+this.currencies[i]].percentChange).toFixed(2);
+              this.ticker[this.market_id+'_'+this.currencies[i]].baseVolume = volume;
+              this.ticker[this.market_id+'_'+this.currencies[i]].percentChange = percentage;
+          } 
+        }
+      }
+      
   }
   doRefresh(refresher) {
-      this.poloniexService.returnTicker().subscribe(data => {
-        //console.log(data);
-        this.ticker = data;
-      });
-      this.poloniexService.returnBalances().subscribe(data => {
-        //console.log(data);
-        this.balances = data;
-      });
+      this.LoadMarket();
       refresher.complete();
   }
   public logout() {
     this.auth.logout().subscribe(succ => {
       this.app.getRootNav().setRoot(LoginPage)
     });
+  }
+  changeMarket(){
+    this.LoadMarket();
   }
 }
