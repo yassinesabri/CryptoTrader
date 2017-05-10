c15f7ebcc7f802355b6fe3400c7a3ec788bcda2a
YASSINE SABRI <yassine.sabri@edu.uca.ma>
2017-05-10 13:17:54 +0100

    change keys card

--- src/pages/deposit/deposits.ts
+++ src/pages/deposit/deposits.ts
@@ -5,41 +5,36 @@ import { NavParams } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
 
 @Component({
-  selector: 'page-deposit',
-  templateUrl: 'deposits.html'
+    selector: 'page-deposit',
+    templateUrl: 'deposits.html'
 })
 export class DepositsPage {
-    action:string;
-    showDep:boolean;
-    showWith:boolean;
-    deposit:any;
-    coin:string;
-    withdrawAddress:string;
-    withdrawAmount:string;
-   constructor(private navParams: NavParams,public poloniexService:PoloniexService) {
-     this.coin="BTC";
-  }
-  changeAction(): void{
-      if(this.action=='Deposit'){
-          this.showDep=true;
-          this.showWith=false;
-          console.log('deposit called');
-    //console.log('key : '+this.poloniexService.apiKey);
-    this.poloniexService.returnDepositAddresses(this.poloniexService.apiKey, this.poloniexService.secretKey).subscribe(data => {
-      //console.log(data);
-      this.deposit = data.result;
-      console.log('hoooooooo',this.deposit);
-     });
-      }
-      else if(this.action=='withdraw'){
-          this.showDep=false;
-          this.showWith=true;
-      }
-    console.log(this.action);
-}
-withdraw():void{
-    console.log('holaw address',this.withdrawAddress);
-    this.poloniexService.withdraw(this.poloniexService.apiKey, this.poloniexService.secretKey, this.coin, this.withdrawAmount,this.withdrawAddress).subscribe(data => {
-        console.log('withdraw log ',data);});
-}
+    action: string;
+    showDep: boolean;
+    showWith: boolean;
+    deposit: any;
+    coin: string;
+    withdrawAddress: string;
+    withdrawAmount: string;
+    constructor(private navParams: NavParams, public poloniexService: PoloniexService) {
+        this.coin = "BTC";
+    }
+    changeAction(): void {
+        if (this.action == 'Deposit') {
+            this.showDep = true;
+            this.showWith = false;
+            console.log('deposit called');
+            //console.log('key : '+this.poloniexService.apiKey);
+            this.poloniexService.returnDepositAddresses(this.poloniexService.apiKey, this.poloniexService.secretKey).subscribe(data => {
+                //console.log(data);
+                this.deposit = data.result;
+                console.log('hoooooooo', this.deposit);
+            });
+        }
+        else if (this.action == 'withdraw') {
+            this.showDep = false;
+            this.showWith = true;
+        }
+        console.log(this.action);
+    }
 }
