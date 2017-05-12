136b892f0bcfe982cd2ab61d788a61c99a756b4b
YASSINE SABRI <yassine.sabri@edu.uca.ma>
2017-05-12 23:19:20 +0100

    color change value in the market dynamically

--- src/pages/market/market.ts
+++ src/pages/market/market.ts
@@ -59,10 +59,17 @@ export class MarketPage implements OnInit {
               let volume = parseFloat(data[this.market_id + '_' + this.currenciesToDisplay[i]].baseVolume).toFixed(3);
               let percentage = parseFloat(data[this.market_id + '_' + this.currenciesToDisplay[i]].percentChange).toFixed(3);
               data[this.market_id + '_' + this.currenciesToDisplay[i]].baseVolume = volume;
-              data[this.market_id + '_' + this.currenciesToDisplay[i]].percentChange = percentage;
+              data[this.market_id + '_' + this.currenciesToDisplay[i]].percentChange = '+'+percentage;
+              if(parseFloat(percentage) > 0){
+                data[this.market_id + '_' + this.currenciesToDisplay[i]].color = "positive";
+              }
+              else{
+                data[this.market_id + '_' + this.currenciesToDisplay[i]].color = "negative";
+              }
             }
           }
         }
+        //console.log(data);
         this.ticker = data;
       });
     }
